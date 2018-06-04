459772306b4293253a7670a2bb0033eec9d5e429
Michiel Koning <mail@michielkoning.nl>
2018-06-04 16:12:12 +0200

    Update service-workers-template.js

--- service-workers-template.js
+++ service-workers-template.js
@@ -1,101 +1,120 @@
-/* global self, caches, fetch, URL, Response */
-'use strict';
+/* global self, $version, $assets, URL, Response */
+
+'use strict'; // eslint-disable-line
 
 const config = {
   version: $version,
   staticCacheItems: $assets,
   cachePathPattern: /^\/(?:(20[0-9]{2}|about|blog|css|images|js)\/(.+)?)?$/,
-  offlineImage: '<svg role="img" aria-labelledby="offline-title"'
-    + ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">'
-    + '<title id="offline-title">Offline</title>'
-    + '<g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>'
-    + '<text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">'
-    + '<tspan x="93" y="172">offline</tspan></text></g></svg>',
+  offlineImage: `
+    <svg role="img" aria-labelledby="offline-title"' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">
+      <title id="offline-title">Offline</title>
+      <g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>
+      <text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">
+      <tspan x="93" y="172">offline</tspan></text></g>
+    </svg>`,
   offlinePage: '/offline/',
 };
 
-const cacheName = (key, opts) => `${opts.version}-${key}`;
+/**
+ * Generate prefixed cache keys
+ *
+ * @param {string} key
+ */
+const cacheName = key => `${config.version}-${key}`;
 
+/**
+ * Cache the response and return it
+ * @param {string} cacheKey
+ * @param {Object} request
+ * @param {Object} response
+ * @returns response
+ */
 const addToCache = (cacheKey, request, response) => {
+  // Don’t cache bad responses.
   if (response.ok) {
+    // Response objects may be used only once.
+    // By cloning it, we are able to create a copy for the cache’s use:
     const copy = response.clone();
-    caches.open(cacheKey).then(cache => cache.put(request, copy));
+    caches.open(cacheKey)
+      .then(cache => cache.put(request, copy));
   }
   return response;
 };
 
-function fetchFromCache (event) {
-  return caches.match(event.request).then(response => {
-    if (!response) {
-      throw Error(`${event.request.url} not found in cache`);
-    }
-    return response;
-  });
-}
+const fetchFromCache = event =>
+  caches.match(event.request)
+    .then((response) => {
+      if (!response) {
+        throw Error(`${event.request.url} not found in cache`);
+      }
+      return response;
+    });
 
-function offlineResponse (resourceType, opts) {
+const offlineResponse = (resourceType) => {
   if (resourceType === 'image') {
-    return new Response(opts.offlineImage,
-      { headers: { 'Content-Type': 'image/svg+xml' } }
+    return new Response(
+      config.offlineImage,
+      {
+        headers: {
+          'Content-Type': 'image/svg+xml',
+        },
+      },
     );
   } else if (resourceType === 'content') {
-    return caches.match(opts.offlinePage);
+    return caches.match(config.offlinePage);
   }
   return undefined;
-}
+};
 
-self.addEventListener('install', event => {
-  function onInstall (event, opts) {
-    var cacheKey = cacheName('static', opts);
+self.addEventListener('install', (event) => {
+  function onInstall(event) {
+    const cacheKey = cacheName('static');
+    const { staticCacheItems } = config;
 
-    opts.staticCacheItems.push('/');
-    opts.staticCacheItems.push(opts.offlinePage);
+    staticCacheItems.push('/');
+    staticCacheItems.push(config.offlinePage);
 
     return caches.open(cacheKey)
-      .then(cache => cache.addAll(opts.staticCacheItems));
+      .then(cache => cache.addAll(staticCacheItems));
   }
 
-  event.waitUntil(
-    onInstall(event, config).then( () => self.skipWaiting() )
-  );
+  event.waitUntil(onInstall(event)
+    .then(() => self.skipWaiting()));
 });
 
-self.addEventListener('activate', event => {
-  function onActivate (event, opts) {
+self.addEventListener('activate', (event) => {
+  function onActivate(event) {
     return caches.keys()
-      .then(cacheKeys => {
-        var oldCacheKeys = cacheKeys.filter(key => key.indexOf(opts.version) !== 0);
-        var deletePromises = oldCacheKeys.map(oldKey => caches.delete(oldKey));
+      .then((cacheKeys) => {
+        const oldCacheKeys = cacheKeys.filter(key => key.indexOf(config.version) !== 0);
+        const deletePromises = oldCacheKeys.map(oldKey => caches.delete(oldKey));
         return Promise.all(deletePromises);
       });
   }
 
-  event.waitUntil(
-    onActivate(event, config)
-      .then( () => self.clients.claim() )
-  );
+  event.waitUntil(onActivate(event)
+    .then(() => self.clients.claim()));
 });
 
-self.addEventListener('fetch', event => {
-
-  function shouldHandleFetch (event, opts) {
-    var request            = event.request;
-    var url                = new URL(request.url);
-    var criteria           = {
-      //matchesPathPattern: opts.cachePathPattern.test(url.pathname),
-      isGETRequest      : request.method === 'GET',
-      isFromMyOrigin    : url.origin === self.location.origin
+self.addEventListener('fetch', (event) => {
+  function shouldHandleFetch(event) {
+    const { request } = event;
+    const url = new URL(request.url);
+    const criteria = {
+      // matchesPathPattern: config.cachePathPattern.test(url.pathname),
+      isGETRequest: request.method === 'GET',
+      isFromMyOrigin: url.origin === self.location.origin,
     };
-    var failingCriteria    = Object.keys(criteria)
+    const failingCriteria = Object.keys(criteria)
       .filter(criteriaKey => !criteria[criteriaKey]);
     return !failingCriteria.length;
   }
 
-  function onFetch (event, opts) {
-    var request = event.request;
-    var acceptHeader = request.headers.get('Accept');
-    var resourceType = 'static';
-    var cacheKey;
+  function onFetch(event) {
+    const { request } = event;
+    const acceptHeader = request.headers.get('Accept');
+    let resourceType = 'static';
 
     if (acceptHeader.indexOf('text/html') !== -1) {
       resourceType = 'content';
@@ -103,26 +122,21 @@ self.addEventListener('fetch', event => {
       resourceType = 'image';
     }
 
-    cacheKey = cacheName(resourceType, opts);
+    const cacheKey = cacheName(resourceType);
 
     if (resourceType === 'content') {
-      event.respondWith(
-        fetch(request)
-          .then(response => addToCache(cacheKey, request, response))
-          .catch(() => fetchFromCache(event))
-          .catch(() => offlineResponse(resourceType, opts))
-      );
+      event.respondWith(fetch(request)
+        .then(response => addToCache(cacheKey, request, response))
+        .catch(() => fetchFromCache(event))
+        .catch(() => offlineResponse(resourceType)));
     } else {
-      event.respondWith(
-        fetchFromCache(event)
-          .catch(() => fetch(request))
-            .then(response => addToCache(cacheKey, request, response))
-          .catch(() => offlineResponse(resourceType, opts))
-      );
+      event.respondWith(fetchFromCache(event)
+        .catch(() => fetch(request))
+        .then(response => addToCache(cacheKey, request, response))
+        .catch(() => offlineResponse(resourceType)));
     }
   }
-  if (shouldHandleFetch(event, config)) {
-    onFetch(event, config);
+  if (shouldHandleFetch(event)) {
+    onFetch(event);
   }
-
 });
